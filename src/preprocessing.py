2f52011a46600b19c1d12f88aba7fc7ed78e42b5
dsayal <157721088+dsayal@users.noreply.github.com>
2024-08-05 15:25:04 -0400

    update column names

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -18,29 +18,27 @@ def load_data():
     '''
     model_pred_df = pd.read_csv('/Users/diyasayal/Desktop/INST414/problem-set-3/data/prediction_model_03.csv')
     genres_df = pd.read_csv('/Users/diyasayal/Desktop/INST414/problem-set-3/data/genres.csv')
+
+    print("Model Predictions Columns:", model_pred_df.columns)
+    print("Genres Columns:", genres_df.columns)
+    
     return model_pred_df, genres_df
+ 
 
 
 def process_data(model_pred_df, genres_df):
-    '''
-    Process data to get genre lists and count dictionaries
+   
+    genre_list = genres_df['genre'].tolist()
     
-    Returns:
-        genre_list (list): List of unique genres
-        genre_true_counts (dict): Dictionary of true genre counts
-        genre_tp_counts (dict): Dictionary of true positive genre counts
-        genre_fp_counts (dict): Dictionary of false positive genre counts
-    '''
-
-    genre_list = genres_df['genre'].unique().tolist()
-
     genre_true_counts = {}
     genre_tp_counts = {}
     genre_fp_counts = {}
-
+    
+    # Initialize counts for each genre
     for genre in genre_list:
-        genre_true_counts[genre] = genres_df[genres_df['genre'] == genre].shape[0]
-        genre_tp_counts[genre] = model_pred_df[(model_pred_df['true_genre'] == genre) & (model_pred_df['predicted_genre'] == genre)].shape[0]
-        genre_fp_counts[genre] = model_pred_df[(model_pred_df['true_genre'] != genre) & (model_pred_df['predicted_genre'] == genre)].shape[0]
-
+        genre_true_counts[genre] = model_pred_df[model_pred_df['actual genres'] == genre].shape[0]
+        genre_tp_counts[genre] = model_pred_df[(model_pred_df['actual genres'] == genre) & (model_pred_df['predicted'] == genre)].shape[0]
+        genre_fp_counts[genre] = model_pred_df[(model_pred_df['actual genres'] != genre) & (model_pred_df['predicted'] == genre)].shape[0]
+    
     return genre_list, genre_true_counts, genre_tp_counts, genre_fp_counts
+
