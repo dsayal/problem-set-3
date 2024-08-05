b42b336713acf8a03ff4b2773d7952f5b82d4be2
dsayal <157721088+dsayal@users.noreply.github.com>
2024-08-05 15:12:26 -0400

    update csv file path

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -16,8 +16,8 @@ def load_data():
         model_pred_df (pd.DataFrame): DataFrame containing model predictions
         genres_df (pd.DataFrame): DataFrame containing genre information
     '''
-    model_pred_df = pd.read_csv('path/to/your/model_predictions.csv')
-    genres_df = pd.read_csv('path/to/your/genres.csv')
+    model_pred_df = pd.read_csv('/Users/diyasayal/Desktop/INST414/problem-set-3/data/model_predictions.csv')
+    genres_df = pd.read_csv('/Users/diyasayal/Desktop/INST414/problem-set-3/data/genres.csv')
     return model_pred_df, genres_df
 
 
